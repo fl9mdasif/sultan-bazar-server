f645785f213517eb98c24e03e5b1fa679632bdcf
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2026-05-04 03:04:24 +0600

    fix: remove dotenv completely, use process.env directly

--- dist/app/config/index.js
+++ dist/app/config/index.js
@@ -1,18 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const path_1 = __importDefault(require("path"));
-// Development এ .env file থেকে আসে
-if (process.env.NODE_ENV !== 'production') {
-    dotenv_1.default.config({ path: path_1.default.join(process.cwd(), '.env') });
-}
+// Development এ .env file থেকে আসে use for local development and production এ environment variable থেকে আসে
+// if (process.env.NODE_ENV !== 'production') {
+//   dotenv.config({ path: path.join(process.cwd(), '.env') });
+// }
 // we got everything from .env by config the file
 exports.default = {
     NODE_ENV: process.env.NODE_ENV,
-    port: process.env.PORT,
+    port: process.env.PORT || 5000,
     database_url: process.env.DATABASE_URL,
     bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
     default_pass: process.env.DEFAULT_PASS,
